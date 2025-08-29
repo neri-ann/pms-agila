ec9bd7ff41907e42390815adfc64d36ab1eea252
neri-ann-username <nerieannsbravo@iskolarngbayan.pup.edu.ph>
2025-08-30 02:30:22 +0800

    db

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); // make sure .env is loaded
 
-  const dbUrl = 'mongodb://127.0.0.1:27017/procument_app'
+const dbUrl = process.env.MONGO_URL;  // use the variable from .env
 
-mongoose.connect(dbUrl) 
+mongoose.connect(dbUrl)
   .then(() => {
-    console.log('DB is connected!');
+    console.log(`DB is connected! (${dbUrl})`);
   })
   .catch((ex) => {
     console.log('DB connection failed: ', ex);
   });
-
-
-
 
 
 //   const mongoose = require('mongoose');
@@ -25,4 +23,3 @@ mongoose.connect(dbUrl)
 //   .catch((ex) => {
 //     console.log('DB connection failed: ', ex);
 //   });
-
